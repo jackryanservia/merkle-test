f2327da87e1e96754743a32c3e3fdb9b85647d84
Jack Ryan Servia <jackryanservia@gmail.com>
2022-02-18 23:58:27 -0700

    POC

--- README.md
+++ README.md
@@ -24,3 +24,33 @@ npm run coverage
 ## License
 
 [Apache-2.0](LICENSE)
+
+# Meeting Notes
+
+### Merkle tree with IPFS and OrbitDB for key value store
+
+I made a simple Mechanisms for committing the merkle root of an OrbitDB keyvaule store to chain and checking inclusion proofs.
+
+##### Questions/Key Points
+
+* Is bit-wise XOR efficient in Mina? Is there any alternative for making proofs that don't require a position vector? Is a bunch of if a bad idea?
+* Would somebody be willing to walk me through the process for adding new stuff to the SnarkJS? Is it better to just make it it's own library? Is there an example of a SnarkyJS library? Are there plans to support interaction between contracts?
+* IPFS seems pretty DDoSable in general. Especially when it comes to updating IPNS entries.
+* Are we going for MVP or something permanent here. It seems like the distance between something that works today and something that makes sense long term could be pretty significant.
+
+### Native IPFS proofs
+
+I believe it's possible to extend IPFS to use the Poseidon hash function! Then it should be possible to create proofs for Poseidon IPFS blocks directly. I don't know exactly what this would look like! I imagine reading state would be simple, but writing state would involve changing one of the blocks on IPFS and updating the onchain root. I haven't found and easy way to do change just one IPFS block yet. IPFS MFS makes it easy to do this at the file level, but implementing all of the MFS stuff in a snark might be too complicated. It's possible to just recreate the whole database from the ground up every time, but it seems like there should be a way to structure the data in IPFS instead of just committing it into a block.
+
+##### Questions/Key Points
+* Are there plans to support SHA?
+* This would be a more general thing that I had originally imagined. It might be a lot more work, but it also might open the door to unique possibilities, and stay useful even if it doesn't become the primary offchain storage solution.
+
+### Mina Validium
+
+![Mina Validium](./MinaOffchain.drawio.png)
+
+##### Questions/Key Points
+
+* Would someone who knows more about everything be willing to talk with me more about this?
+* Is there any plan for how sequencers will work yet?
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,129 @@
-import { Field, SmartContract, state, State, method, UInt64 } from 'snarkyjs';
+import { Field, SmartContract, state, State, method, UInt64, Poseidon, Mina, PrivateKey, Party, isReady, shutdown } from 'snarkyjs';
+import * as IPFS from 'ipfs';
+import OrbitDB from 'orbit-db';
 
-/**
- * Basic Example
- * See https://docs.minaprotocol.com/snapps for more info.
- *
- * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
- * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
- */
-export default class Add extends SmartContract {
-  @state(Field) num = State<Field>();
+let treeDepth = 2;
+
+// go back and maybe organise things into objects
+// make types for OrbitDB and turn strick mode back on for tsc
+// make merkle tree index from 0
+
+await isReady;
+// Mina circuit
+
+export default class Merkle extends SmartContract {
+  @state(Field) root = State<Field>();
 
   // initialization
-  deploy(initialBalance: UInt64, num: Field = Field(1)) {
+  deploy(initialBalance: UInt64) {
     super.deploy();
     this.balance.addInPlace(initialBalance);
-    this.num.set(num);
   }
 
-  @method async update() {
-    const currentState = await this.num.get();
-    const newState = currentState.add(2);
-    newState.assertEquals(currentState.add(2));
-    this.num.set(newState);
+  @method async getMerkleRoot(
+    leaf: Field,              // value of leaf node in question
+    path_values: Field[],     // array of proof values 
+    path_positions: Field[]   // binary vector (0 => lhs, 1 => rhs)
+  ) {
+
+    let merkleRoot = [];
+
+    merkleRoot[0] = Poseidon.hash([
+      leaf.sub(path_positions[0].mul(leaf.sub(path_values[0]))),  // Are bitwise operations efficient? (XOR => Poseidon can remove path_positions maybe). Can I do field addition?
+      path_values[0].sub(path_positions[0].mul(path_values[0].sub(leaf)))]);
+
+    for (let i = 1; i < treeDepth; i++) {
+      merkleRoot[i] = Poseidon.hash([
+        merkleRoot[i - 1].sub(path_positions[i].mul(merkleRoot[i - 1].sub(path_values[i]))),
+        path_values[i].sub(path_positions[i].mul(path_values[i].sub(merkleRoot[i - 1])))]);
+    }
+    return merkleRoot[treeDepth - 1];
   }
 }
+
+// Setup local Mina
+const Local = Mina.LocalBlockchain();
+Mina.setActiveInstance(Local);
+const account1 = Local.testAccounts[0].privateKey;
+const account2 = Local.testAccounts[1].privateKey;
+
+const snappPrivkey = PrivateKey.random();
+let snappAddress = snappPrivkey.toPublicKey();
+
+async function deploy() {
+  let tx = Mina.transaction(account1, async () => {
+    const initialBalance = UInt64.fromNumber(1000000);
+    const p = await Party.createSigned(account2);
+    p.balance.subInPlace(initialBalance);
+    let snapp = new Merkle(snappAddress);
+    snapp.deploy(initialBalance);
+  });
+  await tx.send().wait();
+}
+
+// Initialize IPFS and OrbitDB
+
+const ipfsOptions = { repo: './ipfs', };
+const ipfs = await IPFS.create(ipfsOptions);
+
+const orbitdb = await OrbitDB.createInstance(ipfs);
+
+const db = await orbitdb.keyvalue('merkel-tree');
+
+for (let i = 1; i <= Math.pow(treeDepth, 2); i++) {
+  await putValue(i, 0);
+}
+
+// Client
+
+async function oneLevel(layer) {
+  let result = [];
+  let leaves = [...layer];
+
+  for (let i = 0; i < leaves.length; i+=2) {
+    result.push(Poseidon.hash([new Field(leaves[i]), new Field(leaves[i + 1])]));
+  }
+
+  return result;
+}
+
+async function getRoot() {
+  let values = [];
+
+  for (let i = 1; i <= Math.pow(treeDepth, 2); i++) {
+    let value = await db.get(i.toString());
+    values.push(value);
+  }
+
+  while(values.length > 1) {
+    values = await oneLevel(values);
+  }
+
+  return values[0];
+}
+
+// add BigInt support for value
+async function putValue(index: number, value: number) {
+  await db.put(index.toString(), value);
+  let root = await getRoot();
+
+}
+
+async function getValue(index: number) {
+
+}
+
+// Run stuff
+
+await deploy();
+
+await putValue(1, 5);
+await putValue(2, 6);
+await putValue(3, 7);
+await putValue(4, 8);
+
+
+let root = await getRoot()
+console.log(root);
+
+shutdown();
